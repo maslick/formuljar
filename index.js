a535c83fb3c50960dfa4d39642640954e15722a5
Pavel Maslov <pavel.maslov@tretton37.com>
2020-10-30 18:54:09 +0100

    telegram bot integration

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const axios = require('axios');
 const publicKey = process.env.CAPTCHA_PUBLIC || "6LeqCNkZAAAAAMeqnJ7R2UMdUADc8bdClUOOXDFo";
 const secretKey = process.env.CAPTCHA_SECRET || "6LeqCNkZAAAAAPv_lhQTbsA1sH6vI3ovFgVojoaF";
 
+const token = process.env.BOT_TOKEN || "1318860812:AAHRlDHw4hzLKq382OnhCSOVxKxGKB1lMY4";
+const chatId = process.env.CHAT_ID || "-445121005";
+
 const app = express();
 app.use(express.urlencoded({
   extended: true
@@ -23,8 +26,15 @@ app.post('/form', async function (req, res) {
   const name = req.body["name"];
   if (!result)
     res.send("<div>An error occurred! <a href='/'>Try again</a></div>");
-  else
+  else {
     res.send(`<div>Thank you, ${name}, for your request. We will contact you. <a href='/'>Back</a></div></div><br>`);
+    await sendTelegramMessage({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      message: req.body.message
+    });
+  }
 });
 
 async function verifyToken(token) {
@@ -37,6 +47,27 @@ async function verifyToken(token) {
   } catch (error) {
     console.log(error.response.body);
     return false;
+  }
+}
+
+async function sendTelegramMessage(message) {
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+  const options = {
+    headers: {
+      "content-type": "application/json"
+    }
+  };
+
+  const mess = {
+    chat_id: chatId,
+    text: JSON.stringify(message, null, 4)
+  };
+
+  try {
+    await axios.post(url, mess, options);
+  } catch (error) {
+    console.log(error);
+    console.log(error.response.body);
   }
 }
 
