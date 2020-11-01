b380aa0d6da616eb6e74fb6a5b62bd5d5fd1354a
Pavel Maslov <pavel.maslov@tretton37.com>
2020-11-01 20:56:01 +0100

    cleaned unused stuff

--- sheets.js
+++ sheets.js
@@ -1,32 +1,15 @@
-const fs = require('fs');
-const path = require('path');
 const {google} = require('googleapis');
 const {GOOGLE_CREDENTIALS} = require('./config');
 
 function getAuthConfig() {
-  const client = google.auth.fromJSON(getAuthJson());
+  const client = google.auth.fromJSON(JSON.parse(GOOGLE_CREDENTIALS));
   client.scopes = ['https://www.googleapis.com/auth/spreadsheets'];
   return client;
 }
 
-async function getSpreadSheet({spreadsheetId}) {
-  const sheets = google.sheets({version: 'v4', auth: getAuthConfig()});
-  return await sheets.spreadsheets.get({
-    spreadsheetId
-  });
-}
-
-async function getSpreadSheetValues({spreadsheetId, sheetName}) {
-  const sheets = google.sheets({version: 'v4', auth: getAuthConfig()});
-  return await sheets.spreadsheets.values.get({
-    spreadsheetId,
-    range: sheetName
-  });
-}
-
 async function addEntry({spreadsheetId, sheetName, data}) {
   const sheets = google.sheets({version: 'v4', auth: getAuthConfig()});
-  sheets.spreadsheets.values.append({
+  await sheets.spreadsheets.values.append({
     spreadsheetId,
     range: `${sheetName}`,
     valueInputOption: 'RAW',
@@ -37,12 +20,4 @@ async function addEntry({spreadsheetId, sheetName, data}) {
   })
 }
 
-function getAuthJson() {
-  return JSON.parse(GOOGLE_CREDENTIALS);
-}
-
-module.exports = {
-  getSpreadSheet,
-  getSpreadSheetValues,
-  addEntry
-}
+module.exports = {addEntry}
--- test.js
+++ test.js
@@ -1,30 +1,14 @@
 const assert = require('assert');
 const {google} = require('googleapis');
 const fs = require('fs');
-
-const {
-  getSpreadSheet,
-  getSpreadSheetValues,
-  addEntry
-} = require('./sheets');
+const {addEntry} = require('./sheets');
 
 describe("Google spreadsheets", () => {
   const spreadsheetId = "1uhxQsJ_wzxVbBTwteXH7kaS5IE9LGjw9-KNVZCImdX4";
   const sheetName = "Sheet1";
-
-  it("should fetch spreadsheet name", async () => {
-    const sheet = await getSpreadSheet({spreadsheetId});
-    let speadsheetName = sheet.data.properties['title'];
-    assert.strictEqual(speadsheetName, "formuljar");
-  });
-
-  it("should fetch values", async () => {
-    const vals = await getSpreadSheetValues({spreadsheetId, sheetName});
-    console.log(vals.data.values);
-  });
+  data = ["Nazar", "Ukraine", 37];
 
   it("should add entry", async () => {
-    data = ["Nazar", "Ukraine", 37];
     const vals = await addEntry({spreadsheetId, sheetName, data});
     console.log(vals);
   });
