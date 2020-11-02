91583a36a0490162b2d996b936f97e6ca77a905b
Pavel Maslov <pavel.maslov@tretton37.com>
2020-11-02 19:00:45 +0100

    s3 sync script, removed old stuff

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+: "${API_URL:="https://formuljar-api.maslick.ru"}"
+: "${PUBLIC_KEY:="6LeqCNkZAAAAAMeqnJ7R2UMdUADc8bdClUOOXDFo"}"
+
+mkdir -p dist
+cp ./templates/index.html dist
+rm -f dist/index.html
+
+template=$(<templates/index.html)
+result="${template//%API_URL%/$API_URL}"
+result="${result//%PUBLIC_KEY%/$PUBLIC_KEY}"
+cat <<< "$result" > dist/index.html
+
 aws s3 sync ./dist s3://formuljar-dev
 aws cloudfront create-invalidation --distribution-id E2AODUM7T4OB8O --paths "/index.html"
