171e880b81615e6e6d1a67b31d56c0245a439d16
Pavel Maslov <pavel.maslov@tretton37.com>
2020-11-03 08:03:48 +0100

    added SQS

--- handler.js
+++ handler.js
@@ -1,22 +1,14 @@
 'use strict';
-const {CAPTCHA_PUBLIC} = require("./config");
-const {
-  verifyCaptchaToken,
-  sendTelegramMessage,
-  sendMessageToSheets,
-  successHtml,
-  errorHtml
-} = require("./helper");
-
-const fs = require('fs');
-const path = require('path');
-const querystring = require('querystring')
+const {verifyCaptchaToken, sendTelegramMessage, sendMessageToSheets, successHtml, errorHtml} = require("./helper");
+const querystring = require('querystring');
+const aws = require("aws-sdk");
 
 const headers = {"Content-Type": "text/html"};
+const sqs = new aws.SQS({apiVersion: '2012-11-05'});
 
-module.exports.form = async event => {
+module.exports.form = async (event) => {
   const body = querystring.decode(event.body);
-  console.log(body);
+  console.log("event body: " + JSON.stringify(body));
   const captcha = body["g-recaptcha-response"];
   console.log(captcha);
   const result = await verifyCaptchaToken(captcha);
@@ -27,21 +19,45 @@ module.exports.form = async event => {
     statusCode: 200,
     headers,
     body: errorHtml()
-  }; else {
-    const message = {
-      name: body.name,
-      email: body.email,
-      phone: body.phone,
-      message: body.message
-    };
+  };
 
-    await sendTelegramMessage(message);
-    await sendMessageToSheets(message);
+  const message = {
+    name: body.name,
+    email: body.email,
+    phone: body.phone,
+    message: body.message
+  };
+
+  console.log("Queue url: " + process.env.QUEUE_URL);
+
+  try {
+    await sqs.sendMessage({
+      QueueUrl: process.env.QUEUE_URL,
+      MessageBody: JSON.stringify(message),
+      DelaySeconds: 0,
+    }).promise();
 
     return {
       statusCode: 200,
       headers,
       body: successHtml(name)
     };
+  } catch (err) {
+    console.log(err, err.stack);
+    return {
+      statusCode: 500,
+      headers,
+      body: err
+    };
+  }
+};
+
+module.exports.worker = async event => {
+  console.log("number of messages: " + event.Records.length);
+  for (const value of event.Records) {
+    const mes = JSON.parse(value.body);
+    console.log(mes);
+    await sendTelegramMessage(mes);
+    await sendMessageToSheets(mes);
   }
 };
