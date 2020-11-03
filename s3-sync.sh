b54e1aebc8a532bd0c191f865c63ad1cba33adb1
Pavel Maslov <pavel.maslov@tretton37.com>
2020-11-03 09:16:02 +0100

    added gh actions

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 : "${API_URL:="https://formuljar-api.maslick.ru"}"
-: "${PUBLIC_KEY:="6LeqCNkZAAAAAMeqnJ7R2UMdUADc8bdClUOOXDFo"}"
+: "${CAPTCHA_PUBLIC:="6LeqCNkZAAAAAMeqnJ7R2UMdUADc8bdClUOOXDFo"}"
+: "${CLOUFRONT_ID:="E2AODUM7T4OB8O"}"
+: "${S3_BUCKET:="formuljar-dev"}"
 
 mkdir -p dist
 cp ./templates/index.html dist
@@ -9,8 +11,8 @@ rm -f dist/index.html
 
 template=$(<templates/index.html)
 result="${template//%API_URL%/$API_URL}"
-result="${result//%PUBLIC_KEY%/$PUBLIC_KEY}"
+result="${result//%PUBLIC_KEY%/$CAPTCHA_PUBLIC}"
 cat <<< "$result" > dist/index.html
 
-aws s3 sync ./dist s3://formuljar-dev
-aws cloudfront create-invalidation --distribution-id E2AODUM7T4OB8O --paths "/index.html"
+aws s3 sync ./dist s3://$S3_BUCKET
+aws cloudfront create-invalidation --distribution-id $CLOUFRONT_ID --paths "/index.html"
