8143929f661d821e8afc87031ff327c521f61628
Pavel Maslov <pavel.maslov@tretton37.com>
2020-11-07 12:46:48 +0100

    s3 sync fixes/improvements

--- s3-sync.sh
+++ s3-sync.sh
@@ -2,17 +2,13 @@
 
 : "${API_URL:="https://formuljar-api.maslick.ru"}"
 : "${CAPTCHA_PUBLIC:="6LeqCNkZAAAAAMeqnJ7R2UMdUADc8bdClUOOXDFo"}"
-: "${CLOUFRONT_ID:="E2AODUM7T4OB8O"}"
+: "${CLOUDFRONT_ID:="E2AODUM7T4OB8O"}"
 : "${S3_BUCKET:="formuljar-dev"}"
 
-mkdir -p dist
-cp ./templates/index.html dist
-rm -f dist/index.html
-
-template=$(<templates/index.html)
+template=$(<static/index.html)
 result="${template//%API_URL%/$API_URL}"
 result="${result//%PUBLIC_KEY%/$CAPTCHA_PUBLIC}"
-cat <<< "$result" > dist/index.html
+cat <<< "$result" > static/index.html
 
-aws s3 sync ./dist s3://$S3_BUCKET
-aws cloudfront create-invalidation --distribution-id $CLOUFRONT_ID --paths "/index.html"
+aws s3 sync ./static s3://$S3_BUCKET
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths "/*"
