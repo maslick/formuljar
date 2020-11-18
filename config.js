02ae53480276628e02fe66189490b9cdf10d904a
Pavel Maslov <pavel.maslov@tretton37.com>
2020-11-18 21:33:04 +0100

    fix telegram chat id env var

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3000;
 const CAPTCHA_PUBLIC = process.env.CAPTCHA_PUBLIC || "6LeqCNkZAAAAAMeqnJ7R2UMdUADc8bdClUOOXDFo";
 const CAPTCHA_SECRET = process.env.CAPTCHA_SECRET || "6LeqCNkZAAAAAPv_lhQTbsA1sH6vI3ovFgVojoaF";
 
-const TELEGRAM_CHAT_ID = process.env.CHAT_ID || "-1001355312960"; // formuljar channel
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID || "-1001355312960"; // formuljar channel
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "1318860812:AAHRlDHw4hzLKq382OnhCSOVxKxGKB1lMY4";
 
 const GOOGLE_CREDENTIALS = process.env.GOOGLE_CREDENTIALS || `{
